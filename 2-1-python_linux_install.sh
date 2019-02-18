cbd6eac68c0aa79e32ec053f7a62bb174c10f229
guohongze <guohongze@126.com>
2019-02-18 05:16:04 -0500

    linux convert

--- 2-1-python_linux_install.sh
+++ 2-1-python_linux_install.sh
@@ -2,7 +2,8 @@
 #安装依赖包
 yum -y group install "Development Tools"
 yum install -y wget
-yum -y install openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel vim
+yum -y install openssl-devel bzip2-devel expat-devel \
+gdbm-devel readline-devel sqlite-devel vim
 #下载安装包
 wget https://www.python.org/ftp/python/3.6.8/Python-3.6.8.tar.xz
 #解压
@@ -18,5 +19,7 @@ mv /usr/bin/python /usr/bin/python.bak
 ln -fs /usr/local/python3.6/bin/python3.6 /usr/bin/python
 ln -fs /usr/local/python3.6/bin/pip3.6 /usr/bin/pip
 #替换yum python解释器
-sed -i 's/\#\!\/usr\/bin\/python/\#\!\/usr\/bin\/python2.7/g' /usr/bin/yum
-sed -i 's/\#\!\s\/usr\/bin\/python/\#\!\/usr\/bin\/python2.7/g' /usr/libexec/urlgrabber-ext-down
+sed -i 's/\#\!\/usr\/bin\/python/\#\!\/usr\/bin\/python2.7/g' \
+/usr/bin/yum
+sed -i 's/\#\!\s\/usr\/bin\/python/\#\!\/usr\/bin\/python2.7/g' \
+/usr/libexec/urlgrabber-ext-down
